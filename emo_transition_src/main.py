88c7e2ffc06bc51237d5d43bda28df9ccd3c2054
tazeek <tazeek.rakib@gmail.com>
2023-12-12 18:30:24 +0800

    Update the code

--- emo_transition_src/main.py
+++ emo_transition_src/main.py
@@ -1,21 +1,17 @@
 # coding = utf-8
-import pandas as pd
-import numpy as np
-import torch
+from dataload import load_data
+from model import Emo_Generation
+from train import train_model
 import argparse
-import os
-import datetime
-import traceback
 import model
 
-
-
 # CONFIG
-DATA_PATH = '../data/Dyadic_PELD.tsv'
+DATA_PATH = 'data/Dyadic_PELD.tsv'
 
 parser = argparse.ArgumentParser(description='')
 args = parser.parse_args()
 
+# Load the parameter values
 args.mode         = 3
 args.Senti_or_Emo = 'Emotion'
 args.loss_function= 'Focal' # CE or MSE or Focal
@@ -27,18 +23,16 @@ args.batch_size   = 16
 args.lr           = 1e-4
 args.adam_epsilon = 1e-8
 args.epochs       = 50
-args.result_name  = args.Senti_or_Emo+'_Mode_'+str(args.mode)+'_'+args.loss_function+'_Epochs_'+str(args.epochs)+'.csv'
+
+args.result_name  = args.Senti_or_Emo + '_Mode_' + \
+        str(args.mode) + '_' + args.loss_function + \
+        '_Epochs_' + str(args.epochs) + '.csv'
 
 ## LOAD DATA
-from dataload import load_data
 train_length, train_dataloader, valid_dataloader, test_dataloader = load_data(args, DATA_PATH)
 args.train_length = train_length
 
 ## TRAIN THE MODEL
-from model import Emo_Generation
-from transformers import RobertaConfig, RobertaModel, PreTrainedModel
-from train import train_model
-
 if args.base == 'RoBERTa':
         model = Emo_Generation.from_pretrained('roberta-base', mode=args.mode).cuda(args.device)
 else:
